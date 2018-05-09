820b9cc6c17f8124469ea1c2d5e2ff5dcbe907dd
TyPhyter <tylerjenkins08@gmail.com>
2018-05-09 14:08:07 -0500

    mysql files finished, db created and seeded

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers(
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(150) NOT NULL,
+  devoured TINYINT DEFAULT 0,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) 
+VALUES ('The Cauliflower''s Cumin From Inside the House Burger');
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Poutine on the Ritz Burger');
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Paranormal Pepper Jack-tivity Burger');
