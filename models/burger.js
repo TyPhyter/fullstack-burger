02f4e67833037906afab5c8587f360490af796e4
TyPhyter <tylerjenkins08@gmail.com>
2018-05-09 17:28:05 -0500

    burger model complete

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,26 @@
 const orm = require('../config/orm');
 
 //functions that call on the ORM with burger specific input
+
+const BURG = {
+
+    allBurgers() {
+        return orm.selectAll('burgers');
+    },
+
+    addBurger(name) {
+        return orm.insertOne('burgers', { burger_name: name });
+    },
+
+    eatBurger(name) {
+        return orm.updateOne('burgers', { devoured: true }, { burger_name: name });
+    }
+};
+
+////////////////////
+// USAGE
+////////////////////
+//BURG.allBurgers();
+
+
+module.exports = BURG;
